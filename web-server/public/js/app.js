a86b18e190275a701a18dad084bc17ebed931430
LucasPradellam <lucas.pradella@hotmail.com>
2020-05-23 17:33:14 -0300

    add config port

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -23,7 +23,7 @@ cotacoesform.addEventListener('submit', (event) => {
         mainMensage.innerText = 'O ativo dever ser informado'
     }
 
-    fetch(`http://localhost:3000/cotacoes?ativo=${ativo}`).then((response) =>{
+    fetch(`/cotacoes?ativo=${ativo}`).then((response) =>{
     response.json().then((data) => {
         if (data.error) {
             mainMensage.innerText = `Alguma coisa deu de errado`
