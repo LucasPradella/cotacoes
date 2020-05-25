681a38b8070815393c82c5326d3e6174206de53c
Lucas Pradella <lucas.pradella@hotmail.com>
2020-05-25 15:18:11 -0300

    add todo-api tests

---
+++ todo-api/tests/user.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/users')
+
+beforeAll(async ()=>{
+    await User.deleteMany()
+})
+
+test('Should call GET method successfuly', async() => {
+    await request(app).get('/users').expect(200)
+})
+
+test('Should call POST method successfuly and create a new user', async() => {
+    await request(app)
+            .post('/users')
+            .send({
+                name:"Lucas Pradella",
+                password:"ningueDescobre",
+                age: 29,
+                email:"lucas@pradella.com.br",
+            })
+            .expect(201)
+})
+
+test('Should call GET method and get all Users', async() => {
+    const response = await request(app).get('/users').expect(200)
+    console.log(response.body)
+    expect(response.body[0].name).toBe('Lucas Pradella')
+})
